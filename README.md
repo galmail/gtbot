0f9f8427791be4890ed0e8f552d27c7af01faccb
Gal Dubitski <gal@tocarta.es>
2015-07-27 12:15:51 +0100

    gtbot ready

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # gtbot
+
+This is a simple Node project that sends an email to the seller.
+
+Technology Used: Node.js, Express.js, Phantom.js and Gulp.
+
+To use it, just run:
+
+npm install
+npm start
+
+Then, to send an email, just call the following URL:
+
+http://<host>:<port>/sendmail/<gumtree_id>
--- server.js
+++ server.js
@@ -10,90 +10,55 @@ app.use(express.static(__dirname + '/www', {
   maxAge : 0
 }));
 
+////////// ROUTES //////////
+
+app.get('/', function(req, res) {
+  res.json({"gtbot": "up"});
+});
+
+app.get('/sendmail/:id', function(req, res) {
+  sendmail(req.params.id);
+  res.json({"success": "true"});
+});
+
 ////////// STARTING SERVER //////////
 
 app.listen(process.env.PORT || 3000);
-
 console.log("Node.js is running in " + app.get('env'));
-
 process.on('uncaughtException', function(err) {
   console.log('Caught exception: ' + err);
 });
 
-////////// STARTING MAIN FUNCTION //////////
+////////// MAIN FUNCTION //////////
 
-(function(){
-
-console.log('inside main function...');
-
-// Send email to a gumtree ticket seller
-
-var URL = "http://www.gumtree.com/reply/1125700466";
-
-phantom.create(function(ph){
-  //console.log('ph',ph);
-  ph.createPage(function(page){
-    //console.log('page',page);
-
-    ////////// START - CONSOLE LOGS //////////
-
-    page.onResourceReceived = function(response) {
-      if (response.stage !== "end") return;
-      console.log('Response (#' + response.id + ', stage "' + response.stage + '"): ' + response.url);
-    };
-    page.onResourceRequested = function(requestData, networkRequest) {
-      console.log('Request (#' + requestData.id + '): ' + requestData.url);
-    };
-    page.onUrlChanged = function(targetUrl) {
-      console.log('New URL: ' + targetUrl);
-    };
-    page.onLoadFinished = function(status) {
-      console.log('Load Finished: ' + status);
-    };
-    page.onLoadStarted = function() {
-      console.log('Load Started');
-    };
-    page.onNavigationRequested = function(url, type, willNavigate, main) {
-      console.log('Trying to navigate to: ' + url);
-    };
-
-    ////////// FINISH - CONSOLE LOGS //////////
-
-    console.log('opening page: ' + URL);
-    page.open(URL, function(status){
-      if (status !== 'success') {
-        console.log('Unable to access network');
-      }
-      else {
-        console.log('status', status);
-        //page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
-          //var results = page.evaluate...
+var sendmail = function(gumtreeId){
+  // Send email to a gumtree ticket seller
+  var URL = "http://www.gumtree.com/reply/" + gumtreeId;
+  phantom.create(function(ph){
+    ph.createPage(function(page){
+      console.log('opening page: ' + URL);
+      page.open(URL, function(status){
+        if (status !== 'success') {
+          console.log('Unable to access network');
+        }
+        else {
           page.evaluate(function() {
-            
             var DINGO = {};
             DINGO.MSG = "Hi there, there are people who are looking for tickets like yours on Dingo. You might want to list them, it's super easy and zero commission."
             DINGO.SenderName = "Dingo";
             DINGO.SenderEmail = "hi@dingoapp.co.uk";
-
             document.getElementsByTagName('textarea')[0].value=DINGO.MSG;
             document.getElementsByTagName('input')[3].value=DINGO.SenderName;
             document.getElementsByTagName('input')[4].value=DINGO.SenderEmail;
             document.getElementsByTagName('button')[1].click();
-
           });
-
           setTimeout(function(){
-            page.render("superNextPage.png");
+            //page.render("superNextPage.png");
             ph.exit();
           }, 5000); // 5 seconds
-
-        //});
-      }
+        }
+      });
     });
-
   });
-});
-
-
-})();
+};
 
