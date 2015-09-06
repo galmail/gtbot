a69cf5ffe974b11d499534e1ee28ea40b93f62e9
Gal Dubitski <gal@tocarta.es>
2015-09-06 20:17:28 +0100

    server.js

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ var sendmail_v1 = function(gumtreeId){
   phantom.create(function(ph){
     ph.createPage(function(page){
       console.log('opening page: ' + URL);
+      page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
       page.open(URL, function(status){
         if (status !== 'success') {
           console.log('Unable to access network');
